eaf491e0dd47dec0a4ba39d315a4a4d0cae39592
vuongabk <baovuong158@gmail.com>
2023-09-26 13:07:58 +0700

    Initial commit

---
+++ TourMVC/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TourMVC.Models;
+
+namespace TourMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            //Kiểm tra Email và Password trống?
+            if (string.IsNullOrEmpty(email) == true | string.IsNullOrEmpty(password) == true)
+            {
+                ViewBag.thongbao = "Hãy điền đầy đủ Email và Password";
+                return View();
+            }
+            //Tìm acc trong db
+            var customer = new mapCustomer().TimKiem(email);
+            //Kiểm tra tồn tại
+            if(customer == null)
+            {
+                ViewBag.thongbao = "Email hoặc Password không đúng";
+                ViewBag.email = email;
+                return View();
+            }
+            //Lưu session
+            Session["user"] = customer;
+            //Chuyển về trang chủ
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("user");
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Customer model)
+        {
+            mapCustomer map = new mapCustomer();
+            model.Address = "";
+            if (ModelState.IsValid)
+            {
+                if (map.TimKiem(model.Email) != null)
+                {
+                    ViewBag.thongbao = "Email hoặc Số điện thoại đã tồn tại";
+                    return View(model);
+                }
+                if (map.ThemMoi(model) == true)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+    }
+}
---
+++ TourMVC/Models/mapCustomer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourMVC.Models
+{
+    public class mapCustomer
+    {
+        TourManagementEntities db = new TourManagementEntities();
+
+        public Customer TimKiem(string email)
+        {
+            try
+            {
+                TourManagementEntities db = new TourManagementEntities();
+                var user = db.Customers.SingleOrDefault(m => m.Email.ToLower() == email.ToLower());
+                return user;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public List<Customer> DanhSach()
+        {
+            var users = db.Customers.ToList();
+            return users;
+        }
+
+        public void ThemMoi(string firstName, string lastName, string email, string phone, string password)
+        {
+            Customer tk = new Customer();
+            tk.FirstName = firstName;
+            tk.LastName = lastName;
+            tk.Email = email;
+            tk.Phone = phone;
+            tk.Password = password;
+            db.Customers.Add(tk);
+            db.SaveChanges();
+        }
+
+        public bool ThemMoi(Customer tk)
+        {
+            try
+            {
+                db.Customers.Add(tk);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
